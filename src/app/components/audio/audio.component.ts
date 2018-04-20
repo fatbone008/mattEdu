71b4a14a14d7b26d8921cf46bb911bb120a572b7
陈贻辉 <yihui.chen@esmart365.com>
2018-04-20 17:33:29 +0800

    optimaize mock

--- src/app/components/audio/audio.component.ts
+++ src/app/components/audio/audio.component.ts
@@ -1,7 +1,11 @@
 import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {AudioCounterService} from './audio-counter.service';
 import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/operator/distinct'
+import 'rxjs/add/operator/distinct';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+
+import {Recorder, TextBundler} from '../../models/Recorder';
 
 @Component({
   selector: 'app-audio',
@@ -13,12 +17,33 @@ export class AudioComponent implements OnInit, OnDestroy{
 
   @ViewChild('player') player: ElementRef;
 
-  constructor(private audioCounterServices: AudioCounterService) { }
+  constructor(private audioCounterServices: AudioCounterService) {
+
+  }
 
   ngOnInit() {
-    this.tracer$ = this.audioCounterServices.counter$.distinct();
-    this.audioCounterServices.startTracing(this.player.nativeElement, 500);
+    // mock
+    const recorders = new Recorder([
+      new TextBundler({time: '0:00', text: 'hello'}),
+      new TextBundler({time: '00:34', text: 'Nice to meet you'}),
+      new TextBundler({time: '0:3', text: 'greeting'}),
+      new TextBundler({time: '1:30', text: 'what happen'})
+    ]);
+    const hashingRecorders = recorders.hash();
+    this.tracer$ = this.audioCounterServices.counter$
+      .map(seccond => Math.floor(seccond))
+      .distinct()
+      .do(second => {
+        // console.log('do:', hashingRecorders[second]);
+        if (hashingRecorders[second]) {
+          console.log(hashingRecorders[second]);
+        }
+      });
+    this.audioCounterServices.startTracing(this.player.nativeElement, 700);
     this.tracer$.subscribe(console.log);
+
+
+    console.log(recorders.hash());
   }
 
 
--- src/app/models/Recorder.ts
+++ src/app/models/Recorder.ts
@@ -1,3 +1,31 @@
-export class Recorder{
+export class Recorder {
+  recordObjs: Array<TextBundler>;
 
+  constructor(textBundlers: Array<TextBundler>){
+    this.recordObjs = textBundlers;
+  }
+
+  public hash = () => {
+    let o = {};
+    this.recordObjs.map( textBundler => {
+      o[textBundler.time] = textBundler.text;
+    })
+    return o;
+  }
+}
+
+export class TextBundler {
+  time: number;
+
+  text: string;
+
+  constructor(options) {
+    if (typeof options['time'] === 'string'){
+      const a = options['time'].split(':').map(n => Number(n));
+      this.time = a[0] * 60 + a[1];
+    } else {
+      this.time = 0;
+    }
+    this.text = options['text'] || '';
+  }
 }
