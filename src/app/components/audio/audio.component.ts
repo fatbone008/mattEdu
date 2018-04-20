c74f8d47e3b04584a182b65430b15fb9585ca8c3
陈贻辉 <yihui.chen@esmart365.com>
2018-04-20 18:55:18 +0800

    optimaize mock

--- src/app/components/audio/audio.component.ts
+++ src/app/components/audio/audio.component.ts
@@ -17,33 +17,42 @@ export class AudioComponent implements OnInit, OnDestroy{
 
   @ViewChild('player') player: ElementRef;
 
+  recorders;
+
+  scrollTo: number = 0;
+
   constructor(private audioCounterServices: AudioCounterService) {
 
   }
 
   ngOnInit() {
     // mock
-    const recorders = new Recorder([
+    this.recorders = new Recorder([
       new TextBundler({time: '0:00', text: 'hello'}),
       new TextBundler({time: '00:34', text: 'Nice to meet you'}),
-      new TextBundler({time: '0:3', text: 'greeting'}),
-      new TextBundler({time: '1:30', text: 'what happen'})
+      new TextBundler({time: '0:3', text: 'oh nice!'}),
+      new TextBundler({time: '0:5', text: 'can we talk'}),
+      new TextBundler({time: '0:13', text: 'matt is awesome guy'}),
+      new TextBundler({time: '0:35', text: 'does he married?'}),
+      new TextBundler({time: '0:23', text: 'who knows'}),
+      new TextBundler({time: '0:15', text: 'what happen'})
     ]);
-    const hashingRecorders = recorders.hash();
+    const hashingRecorders = this.recorders.hash();
     this.tracer$ = this.audioCounterServices.counter$
       .map(seccond => Math.floor(seccond))
       .distinct()
       .do(second => {
-        // console.log('do:', hashingRecorders[second]);
+        // 激活的行
         if (hashingRecorders[second]) {
           console.log(hashingRecorders[second]);
+          this.scrollTo = second;
         }
       });
     this.audioCounterServices.startTracing(this.player.nativeElement, 700);
     this.tracer$.subscribe(console.log);
 
 
-    console.log(recorders.hash());
+    console.log(this.recorders.hash());
   }
 
 
